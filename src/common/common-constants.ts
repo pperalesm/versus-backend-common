ccbe00ae37a33d9991e29f79d9e4f474cee611ba
Pablo Perales Molas <pperalesm@gmail.com>
2022-06-17 11:51:15 +0200

    Added review events

--- src/common/common-constants.ts
+++ src/common/common-constants.ts
@@ -3,4 +3,7 @@ export const CommonConstants = {
   ADMIN_ROLE: "ADMIN",
   GAME_CREATED_EVENT: "game_created",
   GAME_DELETED_EVENT: "game_deleted",
+  REVIEW_CREATED_EVENT: "review_created",
+  REVIEW_DELETED_EVENT: "review_deleted",
+  REVIEW_UPDATED_EVENT: "review_updated",
 };
