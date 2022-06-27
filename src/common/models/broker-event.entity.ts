b32c9b3c9fd8eca48161850b94fab6316a6e0581
Pablo Perales Molas <pperalesm@gmail.com>
2022-06-27 12:36:39 +0200

    Fixed ttl for events

--- src/common/models/broker-event.entity.ts
+++ src/common/models/broker-event.entity.ts
@@ -1,16 +1,27 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
 
-@Schema({ expireAfterSeconds: 604800 })
+@Schema({ timestamps: { createdAt: true, updatedAt: false } })
 export class BrokerEvent {
   id?: string;
 
   @Prop({ unique: true })
   uuid?: string;
 
-  constructor({ id, uuid }: { id?: string; uuid?: string }) {
+  createdAt?: Date;
+
+  constructor({
+    id,
+    uuid,
+    createdAt,
+  }: {
+    id?: string;
+    uuid?: string;
+    createdAt?: Date;
+  }) {
     this.id = id;
     this.uuid = uuid;
+    this.createdAt = createdAt;
   }
 }
 
